53d2302ad1bb3e88021daa300e9ca76cb29fa255
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-10-26 21:47:20 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,17 +27,17 @@ macro isexact(expected, expression)
     :($expected == @catcher($expression))
 end
 
-@test @inexact(SafeInt8(0x80))
-@test @exact(SafeUInt8(0xFE), SafeUInt8(0x7F)+SafeUInt8(0x7F))
-@test @overflow(SafeUInt8(0x7F) + SafeUInt8(0x81))
-@test @overflow(SafeUInt8(0x7F) - SafeUInt8(0x81))
+@test @isinexact(SafeInt8(0x80))
+@test @isexact(SafeUInt8(0xFE), SafeUInt8(0x7F)+SafeUInt8(0x7F))
+@test @isoverflow(SafeUInt8(0x7F) + SafeUInt8(0x81))
+@test @isoverflow(SafeUInt8(0x7F) - SafeUInt8(0x81))
 
-@test @exact(SafeInt32(typemax(SafeUInt16)), convert(SafeInt32, ~zero(SafeUInt16)))
-@test @inexact(convert(SafeInt32, ~zero(SafeUInt32)))
+@test @isexact(SafeInt32(typemax(SafeUInt16)), convert(SafeInt32, ~zero(SafeUInt16)))
+@test @isinexact(convert(SafeInt32, ~zero(SafeUInt32)))
 
 @test SafeInt(12) == SafeUInt16(12)
 @test SafeInt(-1) != ~zero(SafeUInt)
 
-@test @exact(SafeUInt8(0xFE >> 2), (SafeUInt8(0x7F)+SafeUInt8(0x7F)) >> 2)
+@test @isexact(SafeUInt8(0xFE >> 2), (SafeUInt8(0x7F)+SafeUInt8(0x7F)) >> 2)
 @test @overflow(SafeUInt8(0x7F) >> SafeUInt8(0x81))
 
