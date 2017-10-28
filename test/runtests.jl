68a497f0acc756db649305eb0a28c1e3b808ac4b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-10-28 02:08:10 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -51,4 +51,13 @@ for T in (:SafeInt8, :SafeInt16, :SafeInt32, :SafeInt64, :SafeInt128)
         @test @overflow(-typemin($T))
         @test @overflow(-typemin($T))
     end
+>>nd<
+for T in (:SafeInt8, :SafeInt16, :SafeInt32, :SafeInt64, :SafeInt128,
+          :SafeUInt8, :SafeUInt16, :SafeUInt32, :SafeUInt64, :SafeUInt128)
+    @eval begin
+        @test @overflow( one($T) >>> (3*sizeof($T)+one($T)) )
+        @test @overflow( one($T) >>  (3*sizeof($T)+one($T)) )
+        @test @overflow( one($T) <<  (3*sizeof($T)+one($T)) )
+    end
 end
+
