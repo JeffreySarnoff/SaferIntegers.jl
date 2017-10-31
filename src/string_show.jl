b7b0c6e48e773c2020a0cd02b644c153f1bb5aa0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-10-31 09:04:25 -0400

    Delete string_show.jl

--- src/string_show.jl
+++
@@ -1,5 +0,0 @@
-Base.string(x::T) where T<:SafeInteger = string( Integer(x) )
-Base.show(io::IO, x::T) where T<:SafeInteger = print(io, string(x) )
-
-Base.hex(n::T, pad::Int=1) where T<:SafeInteger = hex(Integer(n), pad)
-Base.bits(n::T) where T<: SafeInteger = bits(Integer(n))
