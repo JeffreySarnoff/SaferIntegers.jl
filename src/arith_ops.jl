92cb37fd37774b8cdf8c92574740e2b484bb7610
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-10-31 10:40:44 -0400

    Update arith_ops.jl

--- src/arith_ops.jl
+++ src/arith_ops.jl
@@ -19,30 +19,14 @@ for (OP, CHK) in ((:(+), :checked_add), (:(-), :checked_sub),
         @inline function $OP(x::T1, y::T2) where T1<:SafeSigned where T2<:SafeSigned
             xx, yy = promote(x, y)
             return $OP(xx, yy)
-        #= 
-            T = promote_type(T1, T2)
-            I = itype(T)
-            I1 = itype(T1)
-            I2 = itype(T2)
-            r1 = reinterpret(I1, x)%I
-            r2 = reinterpret(I2, y)%I
-            result = $CHK(r1, r2)
-            return reinterpret(T, result)
-        =#
         end
 
         @inline function $OP(x::T1, y::T2) where T1<:SafeUnsigned where T2<:SafeUnsigned
-            T = promote_type(T1, T2)
-            I = itype(T)
-            I1 = itype(T1)
-            I2 = itype(T2)
-            r1 = reinterpret(I1, x)%I
-            r2 = reinterpret(I2, y)%I
-            result = $CHK(r1, r2)
-            return reinterpret(T, result)
+            xx, yy = promote(x, y)
+            return $OP(xx, yy)
         end
-    
-         @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:Integer
+        
+        @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:Integer
             xx, yy = promote(x, y)
             return $OP(xx, yy)
         end
