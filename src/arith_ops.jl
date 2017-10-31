bd2dabebcd5268c24062bd729c17c24b67704bc0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-10-31 10:50:06 -0400

    Update arith_ops.jl

--- src/arith_ops.jl
+++ src/arith_ops.jl
@@ -17,13 +17,23 @@ for (OP, CHK) in ((:(+), :checked_add), (:(-), :checked_sub),
         end
 
         @inline function $OP(x::T1, y::T2) where T1<:SafeSigned where T2<:SafeSigned
+            T = promote_type(T1, T2)
+            I = itype(T)
             xx, yy = promote(x, y)
-            return $OP(xx, yy)
+            r1 = reinterpret(I, xx)
+            r2 = reinterpret(I, yy)
+            result = $CHK(r1, r2)
+            return reinterpret(T, result)
         end
 
         @inline function $OP(x::T1, y::T2) where T1<:SafeUnsigned where T2<:SafeUnsigned
+            T = promote_type(T1, T2)
+            I = itype(T)
             xx, yy = promote(x, y)
-            return $OP(xx, yy)
+            r1 = reinterpret(I, xx)
+            r2 = reinterpret(I, yy)
+            result = $CHK(r1, r2)
+            return reinterpret(T, result)
         end
         
         @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:Integer
