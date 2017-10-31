37047c668543de27b0e9d31318b21b7b2a8de2c8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-10-31 09:02:55 -0400

    Update arith_ops.jl

--- src/arith_ops.jl
+++ src/arith_ops.jl
@@ -8,8 +8,7 @@ for (OP, CHK) in ((:(+), :checked_add), (:(-), :checked_sub),
                   (:fld, :checked_fld), (:cld, :checked_cld),
                   (:rem, :checked_rem), (:mod, :checked_mod))
     @eval begin
-
-       @inline function $OP(x::T, y::T) where T<:SAFEINTEGERS
+       @inline function $OP(x::T, y::T) where T<:SafeInteger
             I = itype(T)
             r1 = reinterpret(I, x)
             r2 = reinterpret(I, y)
@@ -17,7 +16,7 @@ for (OP, CHK) in ((:(+), :checked_add), (:(-), :checked_sub),
             return reinterpret(T, result)
         end
 
-        @inline function $OP(x::T1, y::T2) where T1<:SAFEINTEGERS where T2<:SAFEINTEGERS
+        @inline function $OP(x::T1, y::T2) where T1<:SafeSigned where T2<:SafeSigned
             T = promote_type(T1, T2)
             I = itype(T)
             I1 = itype(T1)
@@ -28,7 +27,7 @@ for (OP, CHK) in ((:(+), :checked_add), (:(-), :checked_sub),
             return reinterpret(T, result)
         end
 
-        @inline function $OP(x::T1, y::T2) where T1<:SAFEINTEGERS where T2<:UNSAFEINTEGERS
+        @inline function $OP(x::T1, y::T2) where T1<:SafeUnsigned where T2<:SafeUnsigned
             T = promote_type(T1, T2)
             I = itype(T)
             I1 = itype(T1)
@@ -38,27 +37,5 @@ for (OP, CHK) in ((:(+), :checked_add), (:(-), :checked_sub),
             result = $CHK(r1, r2)
             return reinterpret(T, result)
         end
-
-        @inline function $OP(x::T2, y::T1) where T1<:SAFEINTEGERS where T2<:UNSAFEINTEGERS
-            T = promote_type(T1, T2)
-            I = itype(T)
-            I2 = itype(T1)
-            I1 = itype(T2)
-            r1 = reinterpret(I1, x)%I
-            r2 = reinterpret(I2, y)%I
-            result = $CHK(r1, r2)
-            return reinterpret(T, result)
-        end
-
-   end
+    end
 end
-
-#=
-# rem, mod conversions
-@inline Base.rem(x::T, ::Type{T}) where {T<:SafeInteger} = T
-@inline Base.rem(x::Integer, ::Type{T}) where {T<:SafeInteger} = SafeInteger(rem(x, itype(T)))
-@inline Base.rem(x::BigInt, ::Type{T}) where {T<:SafeInteger} = error("no rounding BigInt available")
-@inline Base.mod(x::T, ::Type{T}) where {T<:SafeInteger} = T
-@inline Base.mod(x::Integer, ::Type{T}) where {T<:SafeInteger} = SafeInteger(mod(x, itype(T)))
-@inline Base.mod(x::BigInt, ::Type{T}) where {T<:SafeInteger} = error("no rounding mod BigInt available")
-=#
