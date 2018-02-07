3886e8f44cad36637ea5f59cf6946e40fa8829ad
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-07 01:25:35 -0500

    use `signed` instead of `Integer`

--- src/string_io.jl
+++ src/string_io.jl
@@ -1,5 +1,5 @@
-Base.string(x::T) where T<:SafeInteger = string( Integer(x) )
+Base.string(x::T) where T<:SafeInteger = string(signed(x))
 Base.show(io::IO, x::T) where T<:SafeInteger = print(io, string(x) )
 
-Base.hex(n::T, pad::Int=1) where T<:SafeInteger = hex(Integer(n), pad)
-Base.bits(n::T) where T<: SafeInteger = bits(Integer(n))
+Base.hex(n::T, pad::Int=1) where T<:SafeInteger = hex(signed(n), pad)
+Base.bits(n::T) where T<: SafeInteger = bits(signed(n))
