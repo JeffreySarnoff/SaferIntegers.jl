9076edbdf39d84c7d19324cffee5223dd3b90e6a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-14 06:50:57 -0500

    Update construct.jl

--- src/construct.jl
+++ src/construct.jl
@@ -15,3 +15,25 @@ for (S,I) in (
     @inline integer(x::$I) = x
   end
 end
+
+
+for (SS,SU, IS, IU) in (
+    (:SafeInt8, :SafeUInt8, :Int8, :UInt8), 
+    (:SafeInt16, :SafeUInt16, :Int16, :UInt16),
+    (:SafeInt32, :SafeUInt32, :Int32, :UInt32), 
+    (:SafeInt64, :SafeUInt64, :Int64, :UInt64),
+    (:SafeInt128, :SafeUInt128, :Int128, :UInt128) )
+   @eval begin
+     $SS(x::T) where T<:Signed   = safeint(integer($SS)(x))
+     $SU(x::T) where T<:Unsigned = safeint(integer($SS)(x))
+     $SS(x::T) where T<:Unsigned = safeint(integer($SS)(integer($SU)(x)))
+     $SU(x::T) where T<:Signed   = safeint(integer($SU)(integer($SS)(x)))
+
+     $IS(x::T) where T<:SafeSigned   = integer(safeint($IS)(x))
+     $IU(x::T) where T<:SafeUnsigned = integer(safeint($IS)(x))
+     $IS(x::T) where T<:SafeUnsigned = integer(safeint($IS)(safeint($IU)(x)))
+     $IU(x::T) where T<:SafeSigned   = integer(safeint($IU)(safeint($IS)(x)))
+   end
+end
+
+
