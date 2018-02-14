380ed2c88d2e3e4353d734c4d642ebe6841a5344
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-14 04:53:36 -0500

    much cleaner

--- src/arith_ops.jl
+++ src/arith_ops.jl
@@ -1,4 +1,4 @@
-import Base: (+), (-), (*), (/), div, fld, cld, rem, mod
+import Base: (+), (-), (*), (/), (\), div, fld, cld, rem, mod
 import Base.Checked: checked_add, checked_sub, checked_mul,
                      checked_div, checked_fld, checked_cld,
                      checked_rem, checked_mod
@@ -9,31 +9,26 @@ for (OP, CHK) in ((:(+), :checked_add), (:(-), :checked_sub),
                   (:rem, :checked_rem), (:mod, :checked_mod))
     @eval begin
        @inline function $OP(x::T, y::T) where T<:SafeInteger
-            I = itype(T)
-            r1 = reinterpret(I, x)
-            r2 = reinterpret(I, y)
-            result = $CHK(r1, r2)
-            return reinterpret(T, result)
+            ix = integer(x)
+            iy = integer(y)
+            result = $CHK(ix, iy)
+            return safeint(result)
         end
 
         @inline function $OP(x::T1, y::T2) where T1<:SafeSigned where T2<:SafeSigned
             xx, yy = promote(x, y)
-            T = typeof(xx)
-            I = itype(T)
-            r1 = reinterpret(I, xx)
-            r2 = reinterpret(I, yy)
-            result = $CHK(r1, r2)
-            return reinterpret(T, result)
+            ix = integer(xx)
+            iy = integer(yy)
+            result = $CHK(ix, iy)
+            return safeint(result)
         end
 
         @inline function $OP(x::T1, y::T2) where T1<:SafeUnsigned where T2<:SafeUnsigned
             xx, yy = promote(x, y)
-            T = typeof(xx)
-            I = itype(T)
-            r1 = reinterpret(I, xx)
-            r2 = reinterpret(I, yy)
-            result = $CHK(r1, r2)
-            return reinterpret(T, result)
+            ix = integer(xx)
+            iy = integer(yy)
+            result = $CHK(ix, iy)
+            return safeint(result)
         end
         
         @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:Integer
@@ -49,21 +44,43 @@ for (OP, CHK) in ((:(+), :checked_add), (:(-), :checked_sub),
 end
 
 function (/)(x::S, y::S) where S<:SafeInteger
-   d = checked_div(x, y) 
-   return (/)(itype(x), itype(y))
+    ix = integer(x)
+    iy = integer(y)
+    checked_div(ix, iy)
+    result = ix / iy
+    return result
 end
+
+function (\)(x::S, y::S) where S<:SafeInteger
+    ix = integer(y)
+    iy = integer(x)
+    checked_div(ix, iy)
+    result = ix / iy
+    return result
+end
+
 function (/)(x::S1, y::S2) where S1<:SafeInteger where S2<:SafeInteger
    xx, yy = promote(x, y)
-   d = checked_div(xx, yy) 
-   return (/)(itype(xx), itype(yy))
+   return (/)(xx, yy)
 end
 function (/)(x::S1, y::S2) where S1<:SafeInteger where S2<:Integer
    xx, yy = promote(x, y)
-   d = checked_div(xx, yy) 
-   return (/)(itype(xx), itype(yy))
+   return (/)(xx, yy)
 end
 function (/)(x::S1, y::S2) where S2<:SafeInteger where S1<:Integer
    xx, yy = promote(x, y)
-   d = checked_div(xx, yy) 
-   return (/)(itype(xx), itype(yy))
+   return (/)(xx, yy)
+end
+
+function (\)(x::S1, y::S2) where S1<:SafeInteger where S2<:SafeInteger
+   xx, yy = promote(x, y)
+   return (\)(xx, yy)
+end
+function (\)(x::S1, y::S2) where S1<:SafeInteger where S2<:Integer
+   xx, yy = promote(x, y)
+   return (\)(xx, yy)
+end
+function (\)(x::S1, y::S2) where S2<:SafeInteger where S1<:Integer
+   xx, yy = promote(x, y)
+   return (\)(xx, yy)
 end
