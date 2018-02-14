af5637946e8d2eb2aa1bb4e77d93d44f5ced7302
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-14 04:57:31 -0500

    much cleaner

--- src/binary_ops.jl
+++ src/binary_ops.jl
@@ -5,32 +5,26 @@ for OP in (:(<), :(<=), :(>=), :(>), :(!=), :(==), :isless, :isequal)
     @eval begin
 
        @inline function $OP(x::T, y::T) where T<:SafeInteger
-            I = itype(T)
-            r1 = reinterpret(I, x)
-            r2 = reinterpret(I, y)
-            result = $OP(r1, r2)
-            return result
-        end
+           ix = integer(x)
+           iy = integer(y)
+           result = $CHK(ix, iy)
+           return safeint(result)
+       end
 
-        @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:SafeInteger
-            xx, yy = promote(x, y)
-            T = typeof(xx)
-            I = itype(T)
-            r1 = reinterpret(I, xx)
-            r2 = reinterpret(I, yy)
-            result = $OP(r1, r2)
-            return result
-        end
+       @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:SafeInteger
+           xx, yy = promote(x, y)
+           return $OP(xx, yy)
+       end
 
-        @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:Integer
-            xx, yy = promote(x, y)
-            return $OP(xx, yy)
-        end
+       @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:Integer
+           xx, yy = promote(x, y)
+           return $OP(xx, yy)
+       end
 
-        @inline function $OP(x::T1, y::T2) where T1<:Integer where T2<:SafeInteger
-            xx, yy = promote(x, y)
-            return $OP(xx, yy)
-        end
+       @inline function $OP(x::T1, y::T2) where T1<:Integer where T2<:SafeInteger
+           xx, yy = promote(x, y)
+           return $OP(xx, yy)
+       end
    end
 end
 
@@ -38,27 +32,26 @@ for OP in (:(&), :(|), :(⊻), :flipsign, :copysign, :(>>>), :(>>), :(<<))
     @eval begin
 
        @inline function $OP(x::T, y::T) where T<:SafeInteger
-            I = itype(T)
-            r1 = reinterpret(I, x)
-            r2 = reinterpret(I, y)
-            result = $OP(r1, r2)
-            return reinterpret(T, result)
-        end
+           ix = integer(x)
+           iy = integer(y)
+           result = $CHK(ix, iy)
+           return safeint(result)
+       end
 
-        @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:SafeInteger
-            xx, yy = promote(x, y)
-            return $OP(xx, yy)
-        end
+       @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:SafeInteger
+           xx, yy = promote(x, y)
+           return $OP(xx, yy)
+       end
 
        @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:Integer
-            xx, yy = promote(x, y)
-            return $OP(xx, yy)
-        end
+           xx, yy = promote(x, y)
+           return $OP(xx, yy)
+       end
 
-        @inline function $OP(x::T2, y::T1) where T1<:SafeInteger where T2<:Integer
-            xx, yy = promote(x, y)
-            return $OP(xx, yy)
-        end
+       @inline function $OP(x::T2, y::T1) where T1<:SafeInteger where T2<:Integer
+           xx, yy = promote(x, y)
+           return $OP(xx, yy)
+       end
 
    end
 end
