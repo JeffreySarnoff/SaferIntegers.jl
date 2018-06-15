92511fa2c037559cd52f03f297f9e43de73bb921
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-15 07:59:36 -0400

    add type.jl

--- src/SaferIntegers.jl
+++ src/SaferIntegers.jl
@@ -12,37 +12,13 @@ export UInt8, UInt16, UInt32, UInt64, UInt128,
 import Core: UInt8, UInt16, UInt32, UInt64, UInt128,
              Int8, Int16, Int32, Int64, Int128
 
-abstract type SafeInteger  <: Integer end
-abstract type SafeUnsigned <: SafeInteger end
-abstract type SafeSigned   <: SafeInteger end
-
-primitive type SafeInt8    <: SafeSigned     8 end
-primitive type SafeInt16   <: SafeSigned    16 end
-primitive type SafeInt32   <: SafeSigned    32 end
-primitive type SafeInt64   <: SafeSigned    64 end
-primitive type SafeInt128  <: SafeSigned   128 end
-
-primitive type SafeUInt8   <: SafeUnsigned   8 end
-primitive type SafeUInt16  <: SafeUnsigned  16 end
-primitive type SafeUInt32  <: SafeUnsigned  32 end
-primitive type SafeUInt64  <: SafeUnsigned  64 end
-primitive type SafeUInt128 <: SafeUnsigned 128 end
-
-if Sys.WORD_SIZE == 32
-    const SafeInt  = SafeInt32
-    const SafeUInt = SafeUInt32
-else
-    const SafeInt  = SafeInt64
-    const SafeUInt = SafeUInt64
-end
-
-const UnsafeInteger = Union{Signed, Unsigned}
-
+include("type.jl")
 include("construct.jl")
 include("promote.jl")
 include("int_ops.jl")
 include("binary_ops.jl")
 include("arith_ops.jl")
+include("pow.jl")
 include("string_io.jl")
 
 end # module SaferIntegers
