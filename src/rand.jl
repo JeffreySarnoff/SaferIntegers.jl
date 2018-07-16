f1819c06c1ba2d0ee9a8458743399257fec86db7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-16 09:25:55 -0400

    Update rand.jl

--- src/rand.jl
+++ src/rand.jl
@@ -1,18 +1,17 @@
 if VERSION > v"0.7-"
-   import Random.rand
-   const Rnd = Random.rand
+   import Random
+   
+   Random.rand(::Type{T}) where {T<:SafeInteger} = T(rand(integer(T)))
+   Random.rand(::Type{T}, n::I) where {T<:SafeInteger, I<:Integer} = T.(rand(integer(T), n))
+   Random.rand(::Type{T}, n::I) where {T<:SafeInteger, I<:SafeInteger} = T.(rand(integer(T), integer(n)))
+   Random.rand(x::T) where {S<:SafeInteger, T<:UnitRange{S}} = S(rand(integer(x)))
+   Random.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:Integer} = S.(rand(integer(x), n))
+   Random.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:SafeInteger} = S.(rand(integer(x), integer(n)))
 else
-   import Base.rand
-   const Rnd = Base.rand
+   Base.rand(::Type{T}) where {T<:SafeInteger} = T(rand(integer(T)))
+   Base.rand(::Type{T}, n::I) where {T<:SafeInteger, I<:Integer} = T.(rand(integer(T), n))
+   Base.rand(::Type{T}, n::I) where {T<:SafeInteger, I<:SafeInteger} = T.(rand(integer(T), integer(n)))
+   Base.rand(x::T) where {S<:SafeInteger, T<:UnitRange{S}} = S(rand(integer(x)))
+   Base.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:Integer} = S.(rand(integer(x), n))
+   Base.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:SafeInteger} = S.(rand(integer(x), integer(n)))
 end
-
-
-Rnd.rand(::Type{T}) where {T<:SafeInteger} = T(rand(integer(T)))
-Rnd.rand(::Type{T}, n::I) where {T<:SafeInteger, I<:Integer} = T.(rand(integer(T), n))
-Rnd.rand(::Type{T}, n::I) where {T<:SafeInteger, I<:SafeInteger} = T.(rand(integer(T), integer(n)))
-Rnd.rand(x::T) where {S<:SafeInteger, T<:UnitRange{S}} = S(rand(integer(x)))
-Rnd.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:Integer} = S.(rand(integer(x), n))
-Rnd.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:SafeInteger} = S.(rand(integer(x), integer(n)))
-
-
-
