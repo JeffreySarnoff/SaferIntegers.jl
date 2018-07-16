6aadea43c9125ebd25148015f11bb88b3541ea07
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-16 09:21:28 -0400

    Update rand.jl

--- src/rand.jl
+++ src/rand.jl
@@ -1,18 +1,18 @@
 if VERSION > v"0.7-"
    import Random.rand
+   const Rnd = Random.rand
 else
    import Base.rand
+   const Rnd = Base.rand
 end
-export rand
 
-SafeInt32(x::UnitRange{I}) where {I<:Integer} = UnitRange(SafeInt32(x.start), SafeInt32(x.stop))
 
-rand(::Type{T}) where {T<:SafeInteger} = T(rand(integer(T)))
-rand(::Type{T}, n::I) where {T<:SafeInteger, I<:Integer} = T.(rand(integer(T), n))
-rand(::Type{T}, n::I) where {T<:SafeInteger, I<:SafeInteger} = T.(rand(integer(T), integer(n)))
-rand(x::T) where {S<:SafeInteger, T<:UnitRange{S}} = S(rand(integer(x)))
-rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:Integer} = S.(rand(integer(x), n))
-rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:SafeInteger} = S.(rand(integer(x), integer(n)))
+Rnd.rand(::Type{T}) where {T<:SafeInteger} = T(rand(integer(T)))
+Rnd.rand(::Type{T}, n::I) where {T<:SafeInteger, I<:Integer} = T.(rand(integer(T), n))
+Rnd.rand(::Type{T}, n::I) where {T<:SafeInteger, I<:SafeInteger} = T.(rand(integer(T), integer(n)))
+Rnd.rand(x::T) where {S<:SafeInteger, T<:UnitRange{S}} = S(rand(integer(x)))
+Rnd.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:Integer} = S.(rand(integer(x), n))
+Rnd.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:SafeInteger} = S.(rand(integer(x), integer(n)))
 
 
 
