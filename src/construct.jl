58f42e6c8ced8c7cc70f63cbea1e1064d164b4e2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-16 12:32:40 -0400

    Update construct.jl

--- src/construct.jl
+++ src/construct.jl
@@ -68,19 +68,5 @@ for (SS,SU, IS, IU) in (
   end
 end
 
-for T in (:SafeInt8, :SafeInt16, :SafeInt32, :SafeInt64, :SafeInt128,
-          :SafeUInt8, :SafeUInt16, :SafeUInt32, :SafeUInt64, :SafeUInt128)
-    @eval begin
-        $T(x::UnitRange{I}) where {I<:Integer} = UnitRange($T(x.start), $T(x.stop))
-        safeint(x::UnitRange{I}) where {I<:Integer} = $T(x)
-    end
-end
-for T in (:Int8, :Int16, :Int32, :Int64, :Int128,
-          :UInt8, :UInt16, :UInt32, :UInt64, :UInt128)
-    @eval begin
-        $T(x::UnitRange{I}) where {I<:SafeInteger} = UnitRange($T(x.start), $T(x.stop))
-        integer(x::UnitRange{I}) where {I<:SafeInteger} = $T(x)
-    end
-end
-
-
+safeint(x::UnitRange{I}) where {I<:Integer} = safeint(x.start):safeint(x.stop)
+integer(x::UnitRange{S}) where {S<:SafeInteger} = integer(x.start):integer(x.stop)
