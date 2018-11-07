13248164b72446a6e38eb87a7711b71d35d37cf3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-07 09:23:20 -0500

    fixups

--- src/string_io.jl
+++ src/string_io.jl
@@ -1,21 +1,14 @@
-import Base: string, show, hex, bitstring
-
 function string(x::T) where T<:SafeSigned
     str = string(baseint(x))
     return str
 end
 
 function string(x::T) where T<:SafeUnsigned
-    uint = baseint(x)
-    str = string("0x", hex(uint, sizeof(uint)*2))
+    str = string(baseint(x), base=16)
     return str
 end
 
-show(io::IO, x::T) where T<:SafeInteger = print(io, string(x))
+bitstring(n::T) where T<:SafeInteger = bitstring(baseint(n))
+
+show(io::IO, x::T) where T<:SafeInteger = print(io,  string(x))
 show(x::T) where T<:SafeInteger = print(Base.stdout, string(x))
-
-hex(n::T, pad::Int=1) where T<:SafeUnsigned = hex(baseint(n), pad)
-hex(n::T, pad::Int=1) where T<:SafeSigned   = hex(signed(n), pad)
-
-bitstring(n::T) where T<: SafeUnsigned = bitstring(baseint(n))
-bitstring(n::T) where T<: SafeSigned   = bitstring(signed(n))
