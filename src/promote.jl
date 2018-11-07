099b67aa5051c554c6af3d4de72f06da05d7894e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-07 08:59:55 -0500

    baseint

--- src/promote.jl
+++ src/promote.jl
@@ -1,11 +1,11 @@
 import Base: promote_rule
 
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:SafeSigned =
-    safeint(promote_type(integer(S), integer(T)))
+    safeint(promote_type(baseint(S), baseint(T)))
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:SafeUnsigned =
-    safeint(promote_type(integer(S), integer(T)))
+    safeint(promote_type(baseint(S), baseint(T)))
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:SafeUnsigned =
-    safeint(promote_type(integer(S), integer(T)))
+    safeint(promote_type(baseint(S), baseint(T)))
 
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Signed   = S
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Unsigned = S
