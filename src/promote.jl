9acd7f30e1d4ac9a927c311232934e7052b9a489
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-07 08:53:26 -0500

    reformat

--- src/promote.jl
+++ src/promote.jl
@@ -1,13 +1,13 @@
 import Base: promote_rule
 
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned where T<:SafeSigned =
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:SafeSigned =
+    safeint(promote_type(integer(S), integer(T)))
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:SafeUnsigned =
     safeint(promote_type(integer(S), integer(T)))
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:SafeUnsigned =
     safeint(promote_type(integer(S), integer(T)))
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned where T<:SafeUnsigned =
-    safeint(promote_type(integer(S), integer(T)))
 
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned where T<:Signed = S
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Signed   = S
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Unsigned = S
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Signed   = S
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Unsigned = S
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned where T<:Unsigned = S
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Signed = S
