bba776bf65145d4a273e72dd339f21077f5ad9cd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-01-22 14:05:16 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,17 @@
+using Documenter, SaferIntegers
 
+makedocs(
+    modules = [SaferIntegers],
+    sitename = "SaferIntegers.jl",
+    authors = "Jeffrey Sarnoff",
+    pages = Any[
+        "Overview" => "index.md",
+        "How To Use" => "howtouse.md",
+        "Refs" => "references.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/JeffreySarnoff/SaferIntegers.jl.git",
+    target = "build"
+)
