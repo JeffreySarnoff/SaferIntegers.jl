e2674db15b9307baf21eed515565355cee2ba470
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-01-22 23:19:37 -0500

    allocate less

--- src/rand.jl
+++ src/rand.jl
@@ -4,3 +4,5 @@ Random.rand(::Type{T}, n::I) where {T<:SafeInteger, I<:SafeInteger} = T.(rand(ba
 Random.rand(x::T) where {S<:SafeInteger, T<:UnitRange{S}} = S(rand(baseint(x)))
 Random.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:Integer} = S.(rand(baseint(x), n))
 Random.rand(x::T, n::I) where {S<:SafeInteger, T<:UnitRange{S}, I<:SafeInteger} = S.(rand(baseint(x), baseint(n)))
+# allocates less
+Random.rand(ur::UnitRange{SI}, n::Int) where {SI<:SafeInteger} = Random.rand(SI.(Int.(ur)),n)
