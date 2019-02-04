9931eac52f5914b87eaf8222cb80726d8038e568
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-04 14:54:55 -0500

    Update cover.jl

--- src/cover.jl
+++ src/cover.jl
@@ -1,7 +1,6 @@
 for OP in (:checked_abs, :checked_neg, :checked_add, :checked_sub,
-           :checked_mul, :checked_div, :checked_fld, checked_cld,
-           :checked_rem, :checked_mod,
-           add_with_overflow, sub_with_overflow, mul_with_overflow)
+           :checked_mul, :checked_div, :checked_fld, :checked_cld,
+           :checked_rem, :checked_mod)
     @eval begin
        @inline function $OP(x::T, y::T) where T<:SafeInteger
             ix = baseint(x)
@@ -24,6 +23,43 @@ for OP in (:checked_abs, :checked_neg, :checked_add, :checked_sub,
             iy = baseint(yy)
             result = $OP(ix, iy)
             return safeint(result)
+        end
+        
+        @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:Integer
+            xx, yy = promote(x, y)
+            return $OP(xx, yy)
+        end
+
+        @inline function $OP(x::T1, y::T2) where T1<:Integer where T2<:SafeInteger
+            xx, yy = promote(x, y)
+            return $OP(xx, yy)
+        end
+    end
+end
+
+for OP in (:add_with_overflow, :sub_with_overflow, :mul_with_overflow)
+    @eval begin
+       @inline function $OP(x::T, y::T) where T<:SafeInteger
+            ix = baseint(x)
+            iy = baseint(y)
+            value, bool = $OP(ix, iy)
+            return safeint(value), bool
+        end
+
+        @inline function $OP(x::T1, y::T2) where T1<:SafeSigned where T2<:SafeSigned
+            xx, yy = promote(x, y)
+            ix = baseint(xx)
+            iy = baseint(yy)
+            value, bool = $OP(ix, iy)
+            return safeint(value), bool
+        end
+
+        @inline function $OP(x::T1, y::T2) where T1<:SafeUnsigned where T2<:SafeUnsigned
+            xx, yy = promote(x, y)
+            ix = baseint(xx)
+            iy = baseint(yy)
+            value, bool = $OP(ix, iy)
+            return safeint(value), bool
         end
         
         @inline function $OP(x::T1, y::T2) where T1<:SafeInteger where T2<:Integer
