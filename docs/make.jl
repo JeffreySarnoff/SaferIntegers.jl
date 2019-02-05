3b9f4829e2f47dc32380e0a0cf11d762efab1a8c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-04 21:01:01 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -9,8 +9,8 @@ makedocs(
         "Highlights" => "highlights.md",
         "Basic Guide" => "basicguide.md",
         "Supported Operations" => "supports.md",
-        "Symbols Used" => "symbols.md",
         "How To Use" => "howtouse.md",
+        "Safer Rationals" => "rationals.md",
         "Benchmarks" => "benchmarks.md",
         "Refs" => "references.md"
     ]
