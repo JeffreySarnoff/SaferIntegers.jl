4f19472401ec0be6c0209b54fbf625820243339f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-18 21:02:07 -0500

    use testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,75 +36,150 @@ for (S,T) in ((:SafeUInt8, :UInt8), (:SafeUInt16, :UInt16), (:SafeUInt32, :UInt3
    end         
 end
 
-@test SafeInt16(7) >  SafeInt32(2)
-@test SafeInt8(7)  >= SafeInt64(2) 
-@test SafeInt16(2) <  SafeInt32(7)
-@test SafeInt8(2)  <= SafeInt64(7) 
+@testset "constructors" begin
+    @test SafeInt16(Int8(2)) === SafeInt16(2)
+    @test Int16(SafeInt8(2)) === Int16(2)
+    @test SafeUInt64(UInt8(2)) === SafeUInt64(2)
+    @test UInt16(SafeUInt64(2)) === UInt16(2)
+    @test SafeInt16(UInt32(2)) === SafeInt16(2)
+    @test UInt32(SafeInt8(2)) === UInt32(2)
+    @test SafeUInt128(Int8(2)) === SafeUInt128(2)
+    @test Int16(SafeUInt128(2)) === Int16(2)
+end
 
-@test SafeInt32(7) & SafeInt32(2) === SafeInt32(7&2)
-@test SafeInt16(7) | SafeInt16(2) === SafeInt16(7|2)
-@test xor(SafeInt64(7), Int64(2)) === SafeInt64(xor(7,2))
+@testset "comparisons" begin
+    @test SafeInt64(7) ==  SafeInt32(7)
+    @test SafeInt32(7) !=  SafeInt32(2)
+    @test SafeInt16(7) >  SafeInt32(2)
+    @test SafeInt8(7)  >= SafeInt64(2) 
+    @test SafeInt16(2) <  SafeInt32(7)
+    @test SafeInt8(2)  <= SafeInt64(7) 
+    @test isless(SafeInt16(2), SafeInt16(7))
+    @test isequal(SafeInt16(2), SafeInt64(2))
 
-@test SafeInt16(7) + SafeInt32(2) === SafeInt32(7+2)
-@test SafeInt8(7) - SafeInt64(2) === SafeInt64(7-2)
-@test SafeInt32(7) * Int16(2) === SafeInt32(7*2)
+    @test SafeInt64(7) ==  Int32(7)
+    @test SafeInt32(7) !=  Int32(2)
+    @test SafeInt16(7) >   Int32(2)
+    @test SafeInt8(7)  >=  Int64(2) 
+    @test SafeInt16(2) <   Int32(7)
+    @test SafeInt8(2)  <=  Int64(7) 
+    @test isless(SafeInt16(2), Int16(7))
+    @test isequal(SafeInt16(2), Int64(2))
 
-@test SafeInt16(Int8(2)) === SafeInt16(2)
-@test Int16(SafeInt8(2)) === Int16(2)
-@test SafeUInt64(UInt8(2)) === SafeUInt64(2)
-@test UInt16(SafeUInt64(2)) === UInt16(2)
-@test SafeInt16(UInt32(2)) === SafeInt16(2)
-@test UInt32(SafeInt8(2)) === UInt32(2)
-@test SafeUInt128(Int8(2)) === SafeUInt128(2)
-@test Int16(SafeUInt128(2)) === Int16(2)
+    @test Int64(7) ==  SafeInt32(7)
+    @test Int32(7) !=  SafeInt32(2)
+    @test Int16(7) >  SafeInt32(2)
+    @test Int8(7)  >= SafeInt64(2) 
+    @test Int16(2) <  SafeInt32(7)
+    @test Int8(2)  <= SafeInt64(7) 
+    @test isless(Int16(2), SafeInt16(7))
+    @test isequal(Int16(2), SafeInt64(2))
+end
 
-@test Int32(521) + Int32(125) == Int32(SafeInt32(521) + SafeInt32(125))
-@test Int32(521) - Int32(125) == Int32(SafeInt32(521) - SafeInt32(125))
-@test Int32(521) * Int32(125) == Int32(SafeInt32(521) * SafeInt32(125))
-@test div(Int32(521), Int32(125)) == Int32(div(SafeInt32(521), SafeInt32(125)))
+@testset "boolean" begin
+    @test ~SafeUInt32(5) === SafeUInt32(~UInt32(5))
+    @test SafeInt32(7) & SafeInt32(2) === SafeInt32(7&2)
+    @test SafeInt16(7) | SafeInt16(2) === SafeInt16(7|2)
+    @test xor(SafeInt64(7), SafeInt64(2)) === SafeInt64(xor(7,2))
 
-@test SafeInt32(521) + SafeInt32(125) == SafeInt32(Int32(521) + Int32(125))
-@test SafeInt32(521) - SafeInt32(125) == SafeInt32(Int32(521) - Int32(125))
-@test SafeInt32(521) * SafeInt32(125) == SafeInt32(Int32(521) * Int32(125))
-@test div(SafeInt32(521), SafeInt32(125)) == SafeInt32(div(Int32(521), Int32(125)))
+    @test ~SafeUInt32(5) == ~UInt32(5)
+    @test SafeInt32(7) & Int32(2) === SafeInt32(7&2)
+    @test SafeInt16(7) | Int16(2) === SafeInt16(7|2)
+    @test xor(SafeInt64(7), Int64(2)) === SafeInt64(xor(7,2))
 
-@test Int64(521) + Int64(125) == Int64(SafeInt64(521) + SafeInt64(125))
-@test Int64(521) - Int64(125) == Int64(SafeInt64(521) - SafeInt64(125))
-@test Int64(521) * Int64(125) == Int64(SafeInt64(521) * SafeInt64(125))
-@test div(Int64(521), Int64(125)) == Int64(div(SafeInt64(521), SafeInt64(125)))
+    @test Int32(7) & SafeInt32(2) === SafeInt32(7&2)
+    @test Int16(7) | SafeInt16(2) === SafeInt16(7|2)
+    @test xor(Int64(7), SafeInt64(2)) === SafeInt64(xor(7,2))
+end
 
-@test SafeInt64(521) + SafeInt64(125) == SafeInt64(Int64(521) + Int64(125))
-@test SafeInt64(521) - SafeInt64(125) == SafeInt64(Int64(521) - Int64(125))
-@test SafeInt64(521) * SafeInt64(125) == SafeInt64(Int64(521) * Int64(125))
-@test div(SafeInt64(521), SafeInt64(125)) == SafeInt64(div(Int64(521), Int64(125)))
+@testset "arithmetic" begin
+    @test SafeInt16(7) + SafeInt32(2) === SafeInt32(7+2)
+    @test SafeInt8(7) - SafeInt64(2) === SafeInt64(7-2)
+    @test SafeInt32(7) * SafeInt16(2) === SafeInt32(7*2)
+    @test div(SafeInt32(7), SafeInt16(2)) === SafeInt32(div(7,2))
+    @test rem(SafeInt32(7), SafeInt16(-2)) === SafeInt32(rem(7,-2))
+    @test mod(SafeInt32(7), SafeInt16(-2)) === SafeInt32(rem(7,-2))
+    @test fld(SafeInt32(7), SafeInt16(2)) === SafeInt32(fld(7,2))
+    @test cld(SafeInt32(7), SafeInt16(2)) === SafeInt32(cld(7,2))
 
-@test Int64(73)^Int64(5) == Int64(SafeInt64(73)^SafeInt64(5))
-@test SafeInt64(73)^SafeInt64(5) == SafeInt64(Int64(73)^Int64(5))
-@test Int32(73)^Int32(5) == Int64(SafeInt32(73)^SafeInt32(5))
-@test SafeInt32(73)^SafeInt32(5) == SafeInt32(Int32(73)^Int32(5))
+    @test SafeInt16(7) + Int32(2) === SafeInt32(7+2)
+    @test SafeInt8(7) - Int64(2) === SafeInt64(7-2)
+    @test SafeInt32(7) * Int16(2) === SafeInt32(7*2)
+    @test div(SafeInt32(7), Int16(2)) === SafeInt32(div(7,2))
+    @test rem(SafeInt32(7), Int16(-2)) === SafeInt32(rem(7,-2))
+    @test mod(SafeInt32(7), Int16(-2)) === SafeInt32(rem(7,-2))
+    @test fld(SafeInt32(7), Int16(2)) === SafeInt32(fld(7,2))
+    @test cld(SafeInt32(7), Int16(2)) === SafeInt32(cld(7,2))
 
+    @test Int16(7) + SafeInt32(2) === SafeInt32(7+2)
+    @test Int8(7) - SafeInt64(2) === SafeInt64(7-2)
+    @test Int32(7) * SafeInt16(2) === SafeInt32(7*2)
+    @test div(Int32(7), SafeInt16(2)) === SafeInt32(div(7,2))
+    @test rem(Int32(7), SafeInt16(-2)) === SafeInt32(rem(7,-2))
+    @test mod(Int32(7), SafeInt16(-2)) === SafeInt32(rem(7,-2))
+    @test fld(Int32(7), SafeInt16(2)) === SafeInt32(fld(7,2))
+    @test cld(Int32(7), SafeInt16(2)) === SafeInt32(cld(7,2))
 
-@test Int32(521)+Int32(125) == Int32(SafeInt32(521) + SafeInt32(125))
-@test Int32(521)-Int32(125) == Int32(SafeInt32(521) - SafeInt32(125))
+    @test Int32(521) + Int32(125) == Int32(SafeInt32(521) + SafeInt32(125))
+    @test Int32(521) - Int32(125) == Int32(SafeInt32(521) - SafeInt32(125))
+    @test Int32(521) * Int32(125) == Int32(SafeInt32(521) * SafeInt32(125))
+    @test div(Int32(521), Int32(125)) == Int32(div(SafeInt32(521), SafeInt32(125)))
+
+    @test SafeInt32(521) + SafeInt32(125) == SafeInt32(Int32(521) + Int32(125))
+    @test SafeInt32(521) - SafeInt32(125) == SafeInt32(Int32(521) - Int32(125))
+    @test SafeInt32(521) * SafeInt32(125) == SafeInt32(Int32(521) * Int32(125))
+    @test div(SafeInt32(521), SafeInt32(125)) == SafeInt32(div(Int32(521), Int32(125)))
+
+    @test Int64(521) + Int64(125) == Int64(SafeInt64(521) + SafeInt64(125))
+    @test Int64(521) - Int64(125) == Int64(SafeInt64(521) - SafeInt64(125))
+    @test Int64(521) * Int64(125) == Int64(SafeInt64(521) * SafeInt64(125))
+    @test div(Int64(521), Int64(125)) == Int64(div(SafeInt64(521), SafeInt64(125)))
+
+    @test SafeInt64(521) + SafeInt64(125) == SafeInt64(Int64(521) + Int64(125))
+    @test SafeInt64(521) - SafeInt64(125) == SafeInt64(Int64(521) - Int64(125))
+    @test SafeInt64(521) * SafeInt64(125) == SafeInt64(Int64(521) * Int64(125))
+    @test div(SafeInt64(521), SafeInt64(125)) == SafeInt64(div(Int64(521), Int64(125)))
+
+    @test Int64(73)^Int64(5) == Int64(SafeInt64(73)^SafeInt64(5))
+    @test SafeInt64(73)^SafeInt64(5) == SafeInt64(Int64(73)^Int64(5))
+    @test Int32(73)^Int32(5) == Int64(SafeInt32(73)^SafeInt32(5))
+    @test SafeInt32(73)^SafeInt32(5) == SafeInt32(Int32(73)^Int32(5))
+
+    @test Int32(521)+Int32(125) == Int32(SafeInt32(521) + SafeInt32(125))
+    @test Int32(521)-Int32(125) == Int32(SafeInt32(521) - SafeInt32(125))
+end
 
 # shifts
 const bitsof = SaferIntegers.bitsof
 
-@no_error( SafeInt(typemax(Int)) << bitsof(Int) )
-@is_error( SafeInt(typemax(Int)) << (bitsof(Int)+1) )
-@no_error( SafeInt(typemax(Int)) << (-bitsof(Int)) )
-@is_error( SafeInt(typemax(Int)) << (-(bitsof(Int)+1)) )
+@testset "shifts" begin
+    @no_error( SafeInt(typemax(Int)) << bitsof(Int) )
+    @is_error( SafeInt(typemax(Int)) << (bitsof(Int)+1) )
+    @no_error( SafeInt(typemax(Int)) << (-bitsof(Int)) )
+    @is_error( SafeInt(typemax(Int)) << (-(bitsof(Int)+1)) )
 
-@no_error( SafeInt(typemax(Int)) >>> bitsof(Int) )
-@is_error( SafeInt(typemax(Int)) >>> (bitsof(Int)+1) )
-@no_error( SafeInt(typemax(Int)) >>> (-bitsof(Int)) )
-@is_error( SafeInt(typemax(Int)) >>> (-(bitsof(Int)+1)) )
-      
-@test SafeInt32(7) >>> SafeInt32(2) === SafeInt32(7 >>> 2)
-@test SafeInt32(7) >> SafeInt32(2) === SafeInt32(7 >> 2)
-@test SafeInt32(7) << SafeInt32(2) === SafeInt32(7 << 2)
+    @no_error( SafeInt(typemax(Int)) >>> bitsof(Int) )
+    @is_error( SafeInt(typemax(Int)) >>> (bitsof(Int)+1) )
+    @no_error( SafeInt(typemax(Int)) >>> (-bitsof(Int)) )
+    @is_error( SafeInt(typemax(Int)) >>> (-(bitsof(Int)+1)) )
 
-@test typeof(SafeInt32(7) // SafeInt32(5)) === Rational{SafeInt32}
-a = SafeInt32(32); b = SafeInt32(25); c = a//b - 1;
-@test typeof(c) === Rational{SafeInt32}
-@test a//b * b//a === Rational{SafeInt32}(1,1)
+    @test SafeInt32(7) >>> SafeInt32(2) === SafeInt32(7 >>> 2)
+    @test SafeInt32(7) >> SafeInt32(2) === SafeInt32(7 >> 2)
+    @test SafeInt32(7) << SafeInt32(2) === SafeInt32(7 << 2)
+
+    @test SafeInt32(7) >>> Int32(2) === SafeInt32(7 >>> 2)
+    @test SafeInt32(7) >> Int32(2) === SafeInt32(7 >> 2)
+    @test SafeInt32(7) << Int32(2) === SafeInt32(7 << 2)
+
+    @test Int32(7) >>> SafeInt32(2) === SafeInt32(7 >>> 2)
+    @test Int32(7) >> SafeInt32(2) === SafeInt32(7 >> 2)
+    @test Int32(7) << SafeInt32(2) === SafeInt32(7 << 2)
+end
+
+testset "rational" begin
+    a = SafeInt32(32); b = SafeInt32(25); c = a//b - 1;
+
+    @test typeof(SafeInt32(7) // SafeInt32(5)) === Rational{SafeInt32}
+    @test typeof(c) === Rational{SafeInt32}
+    @test a//b * b//a === Rational{SafeInt32}(1,1)
+end
