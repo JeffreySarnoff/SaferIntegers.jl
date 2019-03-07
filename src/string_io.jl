e78b2ff30ecbfdeb384a3065691967d82e436712
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-07 08:09:53 -0500

    improve printing of Unsigneds

--- src/string_io.jl
+++ src/string_io.jl
@@ -10,5 +10,8 @@ end
 
 bitstring(n::T) where T<:SafeInteger = bitstring(baseint(n))
 
-show(io::IO, x::T) where T<:SafeInteger = print(io,  string(x))
-show(x::T) where T<:SafeInteger = print(Base.stdout, string(x))
+show(io::IO, x::T) where T<:SafeSigned = print(io,  string(x))
+show(x::T) where T<:SafeSigned = print(Base.stdout, string(x))
+
+show(io::IO, x::T) where T<:SafeUnsigned = print(io,  baseint(x))
+show(x::T) where T<:SafeUnsigned = print(Base.stdout, baseint(x))
