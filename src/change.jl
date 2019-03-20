97bc2e8db4469fbc5180389159925a17a875d077
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-20 05:02:10 -0400

    Update change.jl

--- src/change.jl
+++ src/change.jl
@@ -1,4 +1,5 @@
 # work with logic from ChangePrecision.jl
+import Base: rand
 
 const HWInt = Union{Int8,Int16,Int32,Int64,Int128,UInt8,UInt16,UInt32,UInt64,UInt128}
 const HWSInt = Union{Int8,Int16,Int32,Int64,Int128}
