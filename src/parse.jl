6388c639561460ad3d190d9fd13e41d6e762a9d5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-19 20:36:25 -0400

    Update parse.jl

--- src/parse.jl
+++ src/parse.jl
@@ -1,9 +1,10 @@
 for (S,T) in ((:SafeInt8, :Int8), (:SafeInt16, :Int16), (:SafeInt32, :Int32),  (:SafeInt64, :Int64), (:SafeInt128, :Int128))
   @eval begin    
-   # function parse(::Type{$S}, str::A) where {A<:AbstractString}
-   #   x = parse($T, str)
-   #   return $S(x)
-   # end
+     function parse(::Type{$S}, str::A) where {A<:AbstractString}
+       occursin(".", str) && return parse(Float64, str)
+       x = parse($T, str)
+       return $S(x)
+     end
     
     function tryparse(::Type{$S}, str::A; base=10) where {A<:AbstractString}
       x = tryparse($T, str, base=Int(base))
