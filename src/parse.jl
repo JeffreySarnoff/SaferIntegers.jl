f5a655be77ff9fbba619cf874e5c6df7681c3559
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-19 20:39:25 -0400

    Update parse.jl

--- src/parse.jl
+++ src/parse.jl
@@ -1,7 +1,7 @@
 for (S,T) in ((:SafeInt8, :Int8), (:SafeInt16, :Int16), (:SafeInt32, :Int32),  (:SafeInt64, :Int64), (:SafeInt128, :Int128))
   @eval begin    
      function parse(::Type{$S}, str::A) where {A<:AbstractString}
-       occursin(".", str) && return parse(Float64, str)
+       (occursin('.', str) || occursin('e', str) || occursin('f', str)) && return Meta.parse(str)
        x = parse($T, str)
        return $S(x)
      end
@@ -15,10 +15,11 @@ end
 
 for (S,T) in ((:SafeUInt8, :UInt8), (:SafeUInt16, :UInt16), (:SafeUInt32, :Int32),  (:SafeUInt64, :UInt64), (:SafeUInt128, :UInt128))
   @eval begin    
-    #function parse(::Type{$S}, str::A) where {A<:AbstractString}
-    #  x = parse($T, str)
-    #  return $S(x)
-    #end
+    function parse(::Type{$S}, str::A) where {A<:AbstractString}
+      (occursin('.', str) || occursin('e', str) || occursin('f', str)) && return Meta.parse(str)
+      x = parse($T, str)
+      return $S(x)
+    end
     
     function tryparse(::Type{$S}, str::A; base=10) where {A<:AbstractString}
       x = tryparse($T, str, base=Int(base))
