ed2e1cea97798c350ace2fe15ea63a909a003ece
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-19 20:33:18 -0400

    Update parse.jl

--- src/parse.jl
+++ src/parse.jl
@@ -1,9 +1,9 @@
 for (S,T) in ((:SafeInt8, :Int8), (:SafeInt16, :Int16), (:SafeInt32, :Int32),  (:SafeInt64, :Int64), (:SafeInt128, :Int128))
   @eval begin    
-    function parse(::Type{$S}, str::A) where {A<:AbstractString}
-      x = parse($T, str)
-      return $S(x)
-    end
+   # function parse(::Type{$S}, str::A) where {A<:AbstractString}
+   #   x = parse($T, str)
+   #   return $S(x)
+   # end
     
     function tryparse(::Type{$S}, str::A; base=10) where {A<:AbstractString}
       x = tryparse($T, str, base=Int(base))
@@ -14,10 +14,10 @@ end
 
 for (S,T) in ((:SafeUInt8, :UInt8), (:SafeUInt16, :UInt16), (:SafeUInt32, :Int32),  (:SafeUInt64, :UInt64), (:SafeUInt128, :UInt128))
   @eval begin    
-    function parse(::Type{$S}, str::A) where {A<:AbstractString}
-      x = parse($T, str)
-      return $S(x)
-    end
+    #function parse(::Type{$S}, str::A) where {A<:AbstractString}
+    #  x = parse($T, str)
+    #  return $S(x)
+    #end
     
     function tryparse(::Type{$S}, str::A; base=10) where {A<:AbstractString}
       x = tryparse($T, str, base=Int(base))
