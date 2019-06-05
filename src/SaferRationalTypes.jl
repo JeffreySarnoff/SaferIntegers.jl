46162cd27b471c70755420d56741ed6099466a90
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-05 02:44:57 -0400

    Update SaferRationalTypes.jl

--- src/SaferRationalTypes.jl
+++ src/SaferRationalTypes.jl
@@ -6,3 +6,20 @@ SafeRational(num::T1, den::T2) where {T1<:Signed, T2<:Signed} = SafeRational(pro
 
 SafeRational(num::T1, den::T2) where {T1<:Signed, T2<:SafeSigned} = SafeRational(promote(safeint(num), den)...)
 SafeRational(num::T1, den::T2) where {T1<:SafeSigned, T2<:Signed} = SafeRational(promote(num, safeint(den))...)
+
+
+SafeRational(num::T, den::T) where {T<:SafeUnsigned} = num//den
+SafeRational(num::T, den::T) where {T<:Unsigned} = safeint(num)//safeint(den)
+
+SafeRational(num::T1, den::T2) where {T1<:SafeUnsigned, T2<:SafeUnsigned} = SafeRational(promote(num, den)...)
+SafeRational(num::T1, den::T2) where {T1<:Unsigned, T2<:Unsigned} = SafeRational(promote(num, den)...)
+
+SafeRational(num::T1, den::T2) where {T1<:Unsigned, T2<:SafeUnsigned} = SafeRational(promote(safeint(num), den)...)
+SafeRational(num::T1, den::T2) where {T1<:SafeUnsigned, T2<:Unsigned} = SafeRational(promote(num, safeint(den))...)
+
+SafeRational(num::T1, den::T2) where {T1<:SafeSigned, T2<:SafeUnsigned} = SafeRational(promote(num, den)...)
+SafeRational(num::T1, den::T2) where {T1<:SafeUnsigned, T2<:SafeSigned} = SafeRational(promote(num, den)...)
+SafeRational(num::T1, den::T2) where {T1<:Unsigned, T2<:SafeSigned} = SafeRational(promote(safeint(num), den)...)
+SafeRational(num::T1, den::T2) where {T1<:SafeSigned, T2<:Unsigned} = SafeRational(promote(num, safeint(den))...)
+SafeRational(num::T1, den::T2) where {T1<:Signed, T2<:SafeUnsigned} = SafeRational(promote(safeint(num), den)...)
+SafeRational(num::T1, den::T2) where {T1<:SafeUnsigned, T2<:Signed} = SafeRational(promote(num, safeint(den))...)
