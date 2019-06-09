ac9026f64b39ef2d06571465f77eaa9e650b80d4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-08 21:23:43 -0400

    CHANGE_TO promote(x::SafeIntNN, y::FloatNN) = FloatNN(x), y

--- src/promote.jl
+++ src/promote.jl
@@ -10,5 +10,14 @@ promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Unsigned = S
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Signed   = S
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Unsigned = S
 
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Base.IEEEFloat = S
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Base.IEEEFloat = S
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Base.IEEEFloat = T
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Base.IEEEFloat = T
+
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:AbstractFloat = T
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:AbstractFloat = T
+
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Real = T
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Real = T
+
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Number = T
+promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Number = T
