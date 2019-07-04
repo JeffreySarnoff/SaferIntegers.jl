220d98e7f1d2609d38e42b05d2a6b31183c16c32
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-04 01:57:48 -0400

    Update promote.jl

--- src/promote.jl
+++ src/promote.jl
@@ -13,11 +13,8 @@ promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Unsigned = S
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Base.IEEEFloat = T
 promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Base.IEEEFloat = T
 
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:AbstractFloat = T
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:AbstractFloat = T
+#promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Base.IEEEFloat = S
+#promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Base.IEEEFloat = S
 
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Real = T
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Real = T
-
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeSigned   where T<:Number = T
-promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Number = T
+promote_rule(::Type{S}, ::Type{BigFloat}) where S<:SafeSigned  = BigFloat
+promote_rule(::Type{S}, ::Type{BigFloat}) where S<:SafeUnsigned = BigFloat
