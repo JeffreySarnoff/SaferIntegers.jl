b5482ebdbdc79df14e25368b148955aec30ba4dc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-11-10 13:45:37 -0800

    Update ipow_table_gen.jl

--- designtime/ipow_table_gen.jl
+++ designtime/ipow_table_gen.jl
@@ -8,7 +8,7 @@
           - result == BigInt(ipow_base)^BigInt(ipow_power)
       
     Each result is determined through exaustive search for overflow
-    using Base.Int128s and DecFP.Dec128s.
+    using Base.Int128s and DecFP.Dec128s. 
     
     The results are independently validated by comparison with 
       the results obtained using a different algorithmic approach
@@ -20,6 +20,36 @@
 
 =#
 
+function pospow_safemax(base::T) where {T}
+    pows = T(1):T(4096)
+    for p in pows
+        pospow_is_unsafe(base, p) && return p-1
+    end
+end
+
+function pospow_unsafemin(base::T) where {T<:Signed}
+    pows = T(4096):T(-1):T(1)
+    for p in pows
+        pospow_is_safe(base, p) && return p+1
+    end    
+    throw(ErrorException("Should not be reached"))
+end
+
+function pospow_is_safe(base::T, power::T) where {T<:Signed}
+    issafe = true
+    tresult = base^power
+    bibase, bipower = BigInt(base), BigInt(power)
+    biresult = bibase^bipower
+    try
+        converted_result = T(biresult)
+    catch
+        issafe = false
+    end    
+    issafe = issafe  & iszero(cmp(biresult,tresult)) & iszero(cmp(converted_result, biresult))
+    return issafe
+end
+
+pospow_is_unsafe(base, power) = !pospow_is_safe(base, power)
 
 using Base.Checked
 #=
