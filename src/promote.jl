8eec6df699bbaa199f718e8fd7038bea5364bca7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-09-02 19:41:58 -0400

    Base.float(x::SafeInteger) for Interpolations.jl (mkitti)

--- src/promote.jl
+++ src/promote.jl
@@ -15,3 +15,7 @@ promote_rule(::Type{S}, ::Type{T}) where S<:SafeUnsigned where T<:Base.IEEEFloat
 
 promote_rule(::Type{S}, ::Type{BigFloat}) where S<:SafeSigned  = BigFloat
 promote_rule(::Type{S}, ::Type{BigFloat}) where S<:SafeUnsigned = BigFloat
+
+Base.float(x::S) where S<:SafeSigned = Base.float(baseint(x))
+Base.float(x::U) where U<:SafeUnsigned = Base.float(baseint(x))
+
