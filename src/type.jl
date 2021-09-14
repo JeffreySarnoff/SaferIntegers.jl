7a44d8c26775561e927ea9e06c0355ee2b4d45fb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-09-13 21:36:18 -0400

    use BitInteger, BitSigned, BitUnsigned from Base

--- src/type.jl
+++ src/type.jl
@@ -22,7 +22,3 @@ else
     const SafeInt  = SafeInt64
     const SafeUInt = SafeUInt64
 end
-
-const UnsafeSigned   = Union{Int128, Int64, Int32, Int16, Int8}
-const UnsafeUnsigned = Union{UInt128, UInt64, UInt32, UInt16, UInt8}
-const UnsafeInteger  = Union{UnsafeSigned, UnsafeUnsigned}
